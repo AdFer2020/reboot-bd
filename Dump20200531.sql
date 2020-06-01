5fe9d2c4962d39e0920a619f948e801e0700c1bb
AdFer2020 <65700849+AdFer2020@users.noreply.github.com>
2020-05-31 19:48:59 -0500

    Create Dump20200531.sql
    backup-bd-project

---
+++ Dump20200531.sql
@@ -0,0 +1,277 @@
+CREATE DATABASE  IF NOT EXISTS `spotify` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `spotify`;
+-- MySQL dump 10.17  Distrib 10.3.23-MariaDB, for Win64 (AMD64)
+--
+-- Host: 64.225.66.83    Database: spotify
+-- ------------------------------------------------------
+-- Server version	10.3.23-MariaDB-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `album`
+--
+
+DROP TABLE IF EXISTS `album`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `album` (
+  `idalbum` int(11) NOT NULL,
+  `genero_album` varchar(20) NOT NULL,
+  `lanzamiento` date NOT NULL,
+  PRIMARY KEY (`idalbum`),
+  CONSTRAINT `idartista` FOREIGN KEY (`idalbum`) REFERENCES `artista` (`idartista`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `album`
+--
+
+LOCK TABLES `album` WRITE;
+/*!40000 ALTER TABLE `album` DISABLE KEYS */;
+/*!40000 ALTER TABLE `album` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `artista`
+--
+
+DROP TABLE IF EXISTS `artista`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `artista` (
+  `idartista` int(11) NOT NULL,
+  `nombre_artista` varchar(45) NOT NULL,
+  `nacionalidad_artista` varchar(45) NOT NULL,
+  PRIMARY KEY (`idartista`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `artista`
+--
+
+LOCK TABLES `artista` WRITE;
+/*!40000 ALTER TABLE `artista` DISABLE KEYS */;
+/*!40000 ALTER TABLE `artista` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `canciones`
+--
+
+DROP TABLE IF EXISTS `canciones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `canciones` (
+  `idcanciones` int(11) NOT NULL,
+  `nombre_cancion` varchar(45) NOT NULL,
+  `duracion` time NOT NULL,
+  `genero` varchar(45) NOT NULL,
+  `fecha_publicacion` date NOT NULL,
+  PRIMARY KEY (`idcanciones`),
+  CONSTRAINT `idcancion` FOREIGN KEY (`idcanciones`) REFERENCES `artista` (`idartista`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `canciones`
+--
+
+LOCK TABLES `canciones` WRITE;
+/*!40000 ALTER TABLE `canciones` DISABLE KEYS */;
+/*!40000 ALTER TABLE `canciones` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favoritos`
+--
+
+DROP TABLE IF EXISTS `favoritos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favoritos` (
+  `idfavoritos` int(11) NOT NULL,
+  PRIMARY KEY (`idfavoritos`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favoritos`
+--
+
+LOCK TABLES `favoritos` WRITE;
+/*!40000 ALTER TABLE `favoritos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `favoritos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `login` (
+  `idlogin` int(11) NOT NULL,
+  `feha_fin` date NOT NULL,
+  PRIMARY KEY (`idlogin`),
+  CONSTRAINT `idlogin` FOREIGN KEY (`idlogin`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paga`
+--
+
+DROP TABLE IF EXISTS `paga`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `paga` (
+  `idpaga` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  `comprobante_pago` varchar(45) NOT NULL,
+  PRIMARY KEY (`idpaga`),
+  CONSTRAINT `idservicios` FOREIGN KEY (`idpaga`) REFERENCES `servicios` (`idservicios`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `idusuario` FOREIGN KEY (`idpaga`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paga`
+--
+
+LOCK TABLES `paga` WRITE;
+/*!40000 ALTER TABLE `paga` DISABLE KEYS */;
+/*!40000 ALTER TABLE `paga` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `idroles` int(11) NOT NULL,
+  PRIMARY KEY (`idroles`),
+  CONSTRAINT `idroles` FOREIGN KEY (`idroles`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `seleccion`
+--
+
+DROP TABLE IF EXISTS `seleccion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `seleccion` (
+  `idseleccion` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `hora` time NOT NULL,
+  PRIMARY KEY (`idseleccion`),
+  CONSTRAINT `idcanciones` FOREIGN KEY (`idseleccion`) REFERENCES `canciones` (`idcanciones`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `seleccion`
+--
+
+LOCK TABLES `seleccion` WRITE;
+/*!40000 ALTER TABLE `seleccion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `seleccion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicios`
+--
+
+DROP TABLE IF EXISTS `servicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `servicios` (
+  `idservicios` int(11) NOT NULL,
+  `de_paga` char(2) NOT NULL,
+  `limitada` char(2) NOT NULL,
+  `premium` char(2) NOT NULL,
+  PRIMARY KEY (`idservicios`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idusuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(45) NOT NULL,
+  `apellido_usuario` varchar(45) NOT NULL,
+  `correo` varchar(60) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  PRIMARY KEY (`idusuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-31 19:29:08
